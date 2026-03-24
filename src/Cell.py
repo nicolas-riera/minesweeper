e24f63c54d7d027400952549dbc21ed95f031b81
nicolas-riera <nicolas.riera@laplateforme.io>
2026-03-24 15:13:02 +0100

    other: removed unused comment

--- src/Cell.py
+++ src/Cell.py
@@ -5,12 +5,3 @@ class Cell:
         self.questionmark = False
         self.is_dug = False
         self.surrounding_bombs = 0 # up to 8
-    
-    # def grid(self, rows, columns):
-    #     self.column = columns
-    #     self.row = rows
-
-        # grid = []
-
-        # def grid(self):
-            
