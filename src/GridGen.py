fc75968833348e9af99f8e3b3ac7aebbf6e1ca16
hugo-belaloui <hugo.belaloui@laplateforme.io>
2026-03-24 11:53:30 +0100

    feat(grid): implement basic mine spawning logic
      Added a mines_spawning method to GridGen that randomly places three mines on the grid for testing purposes. Updated the debug output to verify the total mine count.

--- src/GridGen.py
+++ src/GridGen.py
@@ -10,10 +10,25 @@ class GridGen:
                 new_rows.append(Case())
             self.grid.append(new_rows)
 
+    def mines_spawning(self):
+        mines_amount = 0 #up to three for testing purposes
+        while mines_amount < 3:
+            r_random = random.randint(0, 7)
+            c_random = random.randint(0, 7)
+
+            if not self.grid[r_random][c_random].mine:
+                self.grid[r_random][c_random].mine = True
+                mines_amount +=1
+
+
+
 
 #print debug
 
 game = GridGen()
+game.mines_spawning()
 
 print("Nombre de lignes :", len(game.grid))
 print("Nombre de colonnes dans la première ligne :", len(game.grid[0]))
+sum_bomb = sum(case.mine for row in game.grid for case in row)
+print("Nombre de bombes :", sum_bomb)
