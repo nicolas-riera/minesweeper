78e956ac31b9718bb23bc9c024febed79a7d0fb5
nicolas-riera <nicolas.riera@laplateforme.io>
2026-03-25 10:49:01 +0100

    fix: stop bombs from spawning around the first cases clicked on

--- src/GridGen.py
+++ src/GridGen.py
@@ -16,15 +16,18 @@ class GridGen:
                 new_rows.append(Cell()) #add to each cell an object cell
             self.grid.append(new_rows)
 
-    def mines_spawning(self, nb, length, row=None, col=None):
-        
+    def mines_spawning(self, nb, length, row, col):
 
         mines_amount = 0 
         while mines_amount < nb: 
             r_random = random.randint(0, length-1) 
             c_random = random.randint(0, length-1)
 
-            if not self.grid[r_random][c_random].mine and not self.grid[r_random][c_random].is_dug and not [self.grid[row+s[0]][col+s[1]] for s in surrounding]: #as long as the cell isn't dug nor a mine and as long as the amount of mine required wasn't planted keep planting on random cells
+            if (
+                not self.grid[r_random][c_random].mine and not self.grid[r_random][c_random].is_dug and
+                self.grid[r_random][c_random]
+                not in [self.grid[row+s[0]][col+s[1]] for s in surrounding]
+            ): #as long as the cell isn't dug nor a mine and as long as the amount of mine required wasn't planted keep planting on random cells
                 self.grid[r_random][c_random].mine = True
                 mines_amount +=1
     
